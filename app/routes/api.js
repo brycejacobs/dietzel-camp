e5d979892175eb8376c400b982ee71aa1d49ebef
Bryce Jacobs <popasquat89@gmail.com>
2013-06-26 18:16:54 -0500

    Changed package.json

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,5 +4,9 @@ exports.index = function(req, res) {
   res.render('map');
 };
 
+app.get('*', function (req, res) { // redirect all others to the index (HTML5 history)
+  req.url = '/';
+});
+
 app.get('/map', exports.index);
 app.get('/', exports.index);
