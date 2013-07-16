84cec9cff40f889794abdce8e70f7d7b13be75e4
Bryce Jacobs <popasquat89@gmail.com>
2013-07-16 13:08:03 -0500

    Cut registration down to one button per client request

--- app/controllers/maps.js
+++ app/controllers/maps.js
@@ -1,6 +1,55 @@
-
+var   User = require('./../models/user')
+    , check = require('validator').check;
 
 exports.onSave = function onSaveUserData (req, res) {
-  console.log(res.body);
-  return res.send({code: 200});
+  var   email
+      , geo
+      , name
+      , sentData = req.body;
+
+  try {
+    email = sentData.email;
+    check(email).isEmail().notNull();
+  } catch (e) {
+    //no email
+    email = null;
+  }
+
+  try {
+    name = sentData.name;
+    check(name).notNull();
+  } catch (e) {
+    name = null;
+  }
+
+  try {
+    geo = sentData.geoLocation;
+    check(geo).notNull();
+  } catch (e) {
+    geo = null;
+  }
+
+  new User({
+    fromInput: {
+      address: sentData.address,
+      email: email,
+      googleLocation: {
+        latitude: sentData.userLocation.lat,
+        longitude: sentData.userLocation.long
+      },
+      name: name
+    },
+    geoData: {
+      latitude: geo.lat,
+      longitude: geo.long
+    }
+
+  }).save(function onSaveComplete (error) {
+    if(error) {
+      console.log('Error:\n%s', error);
+      return res.send(500, error);
+    } else {
+      return res.send(200);
+    }
+  });
 };
--- app/models/user.js
+++ app/models/user.js
@@ -2,14 +2,20 @@ var   mongoose = require('mongoose')
     , Schema = mongoose.Schema;
 
 var User = new Schema({
-  created: {type: Date, default: Date.now()}
-  email: {type: String, default: null},
-  name: {type: String, default: null},
+  created: {type: Date, default: Date.now()},
+  fromInput: {
+    address: {type: String, default: null},
+    email: {type: String, default: null},
+    googleLocation: {
+      latitude: {type: Number, default: null},
+      longitude: {type: Number, default: null}
+    },
+    name: {type: String, default: null}
+  },
   geoData: {
     latitude: {type: Number, default: null},
     longitude: {type: Number, default: null}
   }
-  zipCode: {type: Number, default: null}
 });
 
 module.exports = mongoose.model('User', User);
