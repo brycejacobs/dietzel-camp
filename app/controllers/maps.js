688d2f0ab807bc7fead74a2a02aa450a62b043c7
Bryce Jacobs <popasquat89@gmail.com>
2013-07-16 18:22:41 -0500

    fixed router, added admin export

--- app/controllers/maps.js
+++ app/controllers/maps.js
@@ -1,6 +1,8 @@
 var   User = require('./../models/user')
     , check = require('validator').check
-    , Q = require('q');
+    , Q = require('q')
+    , _ = require('lodash')
+    , nodeExcel = require('excel-export');
 
 exports.onSave = function onSaveUserData (req, res) {
   var   email
@@ -56,7 +58,6 @@ exports.onSave = function onSaveUserData (req, res) {
 };
 
 exports.onAggregate = function (req, res) {
-  console.log('here');
   User.find({})
     .exec()
     .then(function onFindUsersComplete (users) {
@@ -64,6 +65,53 @@ exports.onAggregate = function (req, res) {
     }, function onFindUsersFail (error) {
       return res.send(500, error);
     });
+};
+
+exports.onExportXLS = function (req, res) {
+  User.find({})
+    .exec()
+    .then(function onFindUsersComplete (users) {
+      console.log(users);
+      return callback(users);
+    }, function onFindUsersFail (error) {
+      return res.send(500, error);
+    });
+
+  function callback(result) {
+    var conf = {};
+    conf.cols = [
+      {caption: 'Address', type: 'string'},
+      {caption: 'Name', type: 'string'},
+      {caption: 'Email', type: 'string'},
+      {caption: 'Hit-Date', type: 'date'},
+      {caption: 'Geo-Lat', type: 'string'},
+      {caption: 'Geo-Lng', type: 'string'},
+      {caption: 'Addr-Lat', type: 'string'},
+      {caption: 'Addr-Lng', type: 'string'},
+    ];
+
+    var rows = [];
+
+    _.forEach(result, function onIterateUsers (user, index) {
+      var row = [];
+      row.push(user.fromInput.address);
+      row.push(user.fromInput.name || 'N/A');
+      row.push(user.fromInput.email || 'N/A');
+      row.push(new Date(user.created).getJulian());
+      row.push(user.geoData.latitude || 'N/A');
+      row.push(user.geoData.longitude || 'N/A');
+      row.push(user.fromInput.googleLocation.latitude || 'N/A');
+      row.push(user.fromInput.googleLocation.longitude || 'N/A');
+      rows.push(row);
+    });
+
+    conf.rows = rows;
+
+    var result = nodeExcel.execute(conf);
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+    res.setHeader("Content-Disposition", "attachment; filename=" + "Dietzel For Congress Signups.xlsx");
+    res.end(result, 'binary');
+  }
 };
 
 
--- app/routes/api.js
+++ app/routes/api.js
@@ -2,10 +2,13 @@ app = module.parent.exports.app;
 
 var maps = require('./../controllers/maps');
 
-app.get('*', function (req, res) { // redirect all others to the index (HTML5 history)
-  req.url = '/';
-});
+
 
 app.post('/api/map/save', maps.onSave);
 
+app.get('/api/users/export', maps.onExportXLS);
 app.get('/api/users', maps.onAggregate);
+
+app.get('*', function (req, res) { // redirect all others to the index (HTML5 history)
+  req.url = '/';
+});
